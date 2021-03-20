43fc8ad249e3defcba4ac8db87e9476606333f69
Victor Kozyrev <victor.kozyrev@powerplan.com>
2021-03-20 17:01:23 -0400

    updated error handling; code refactoring

---
+++ Workers/ErrorWorker.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Fb2CleanerApp.Workers
+{
+    public class ErrorWorker
+    {
+        private readonly string _file;
+        private string _text;
+        private bool _dirty;
+        private Encoding _encoding;
+
+        public ErrorWorker(string file)
+        {
+            _file = file;
+        }
+        private static readonly HashSet<string> Exclude = new() { "&amp;", "&apos;", "&quot;", "&gt;", "&lt;" };
+        private static readonly Regex ExcludeSearchPattern = new Regex(@"&[\d\w]+?;", RegexOptions.Compiled);
+
+        private static string HtmlEscapesToXml(string text, List<Match> matches)
+        {
+            foreach (var match in matches)
+            {
+                var special = match.Value;
+                var xmlSpecial = System.Web.HttpUtility.HtmlDecode(special);
+                text = text
+                    .Remove(match.Index, match.Length)
+                    .Insert(match.Index, xmlSpecial);
+            }
+            return text;
+        }
+
+        private void GuessEncoding()
+        {
+            using var fs = File.OpenRead(_file);
+            var charsetDetector = new Ude.CharsetDetector();
+            charsetDetector.Feed(fs);
+            charsetDetector.DataEnd();
+            _encoding = charsetDetector.Charset != null ? Encoding.GetEncoding(charsetDetector.Charset) : Encoding.UTF8;
+        }
+
+        public void CorrectNamespaces()
+        {
+            const string xlinkNamespace = @"xmlns:xlink=""http://www.w3.org/1999/xlink""";
+            const string xlink = "<a xlink:href=";
+
+            if (!_text.Contains(xlink)) return;
+
+            var match = Regex.Match(_text, "\\<FictionBook\\s([\\w\\W]+?)\\>");
+            if (!match.Success) return;
+
+            var attributes = Regex.Split(match.Groups[1].Value, "\\s+");
+
+            if (attributes.Contains(xlinkNamespace)) return;
+
+            var position = match.Groups[1].Index + match.Groups[1].Length;
+            _text = _text.Insert(position, " " + xlinkNamespace);
+            _dirty = true;
+        }
+
+        public async Task ReadFromFile()
+        {
+            GuessEncoding();
+            _text = await File.ReadAllTextAsync(_file, _encoding);
+            _dirty = false;
+        }
+
+        public async Task SaveToFile()
+        {
+            if (_dirty)
+            {
+                // rename the old file
+                File.SetAttributes(_file, FileAttributes.Normal);
+                File.Move(_file, _file + "_", true);
+
+                // save the corrected file
+                await File.WriteAllTextAsync(_file, _text, _encoding);
+                _dirty = false;
+            }
+        }
+
+        //public void FixHrefLinkIssue()
+        //{
+        //    const string badLink = "<a xlink:href=";
+        //    const string goodLink = "<a href=";
+
+        //    if (!_text.Contains(badLink)) return;
+        //    _text = _text.Replace(badLink, goodLink);
+        //    _dirty = true;
+        //}
+
+        public void FixEmphasisTagIssue()
+        {
+            const string badStart = "<p><emphasis><emphasis></emphasis></p>";
+            const string badEnd = "<p><emphasis></emphasis></emphasis></p>";
+            const string goodStart = "<p><emphasis></emphasis></p>";
+            const string goodEnd = "<p><emphasis></emphasis></p>";
+
+            if (!_text.Contains(badStart) && !_text.Contains(badEnd)) return;
+            _text = _text
+                .Replace(badStart, goodStart)
+                .Replace(badEnd, goodEnd);
+            _dirty = true;
+        }
+        public void FixHtmlEscapeCharacterIssue()
+        {
+            var matches = ExcludeSearchPattern
+                .Matches(_text)
+                .Where(m => !Exclude.Contains(m.Value))
+                .Reverse()
+                .ToList();
+            if (matches.Count == 0) return;
+            _text = HtmlEscapesToXml(_text, matches);
+            _dirty = true;
+        }
+    }
+}
---
+++ Workers/Fb2Worker.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Fb2.Document;
+using Fb2.Document.Models;
+using Fb2.Document.Models.Base;
+using Fb2CleanerApp.Models;
+
+namespace Fb2CleanerApp.Workers
+{
+    public class Fb2Worker
+    {
+        private readonly string _file;
+
+
+        public Fb2Worker(string file)
+        {
+            _file = file;
+        }
+
+        #region Static Methods
+
+        private static void ParseTitleInfo(BookSummary bookSummary, IEnumerable<Fb2Node> nodes)
+        {
+            foreach (var node in nodes)
+            {
+                switch (node)
+                {
+                    case BookGenre genre:
+                        bookSummary.Genres.Add(genre.ToString());
+                        break;
+                    case Author author:
+                        bookSummary.Authors.Add(author);
+                        var firstName = author.Content.FirstOrDefault(x => x is FirstName)?.ToString() ?? string.Empty;
+                        var lastName = author.Content.FirstOrDefault(x => x is LastName)?.ToString() ?? string.Empty;
+                        var name = string.Empty;
+                        if (!string.IsNullOrWhiteSpace(firstName))
+                        {
+                            name = firstName;
+                        }
+
+                        if (!string.IsNullOrWhiteSpace(lastName))
+                        {
+                            if (!string.IsNullOrEmpty(name))
+                            {
+                                name += " ";
+                            }
+
+                            name += lastName;
+                        }
+
+                        bookSummary.AuthorNames.Add(name);
+                        break;
+                    case BookTitle title:
+                        bookSummary.Title = title.ToString();
+                        break;
+                    case SequenceInfo seq:
+                        bookSummary.SeriesName = seq.Attributes["name"];
+                        var number = 0;
+                        if (seq.Attributes.ContainsKey("number"))
+                        {
+                            int.TryParse(seq.Attributes["number"], out number);
+                        }
+
+                        bookSummary.SeriesNumber = number;
+                        break;
+                }
+            }
+        }
+
+        private static void ParseBodyInfo(BookSummary bookSummary, IEnumerable<Fb2Node> nodes)
+        {
+            foreach (var node in nodes)
+            {
+                switch (node)
+                {
+                    case BodySection section:
+                        ParseBodyInfo(bookSummary, section.Content);
+                        break;
+                    case Title title:
+                        bookSummary.Chapters.Add(title.ToString().Trim('\r').Trim('\n'));
+                        break;
+                }
+            }
+        }
+
+        #endregion
+
+        public async Task<BookSummary> Parse()
+        {
+            BookSummary bookSummary;
+            try
+            {
+                var document = new Fb2Document();
+                await using var stream = File.OpenRead(_file);
+                await document.LoadAsync(stream);
+                bookSummary = new BookSummary
+                {
+                    Origin = _file,
+                    FileName = _file,
+                    Zipped = false,
+                    Document = document
+                };
+                Fb2Worker.ParseTitleInfo(bookSummary, bookSummary.Document.Title.Content);
+                Fb2Worker.ParseBodyInfo(bookSummary, bookSummary.Document.Bodies
+                    .SelectMany(x => x.Content)
+                    .ToList());
+            }
+            catch (Exception x)
+            {
+                bookSummary = new BookSummary
+                {
+                    Origin = _file,
+                    FileName = _file,
+                    Zipped = false,
+                    InvalidFormat = true,
+                    Exception = x
+                };
+            }
+
+            return bookSummary;
+        }
+
+        public async Task<List<string>> ExtractZipped()
+        {
+            var files = new List<string>();
+            var prefix = Path.GetFileNameWithoutExtension(_file) + "_";
+            using var archive = ZipFile.Open(_file ?? string.Empty, ZipArchiveMode.Read, Encoding.UTF8);
+            foreach (var entry in archive.Entries.Where(x => x.FullName.EndsWith(".fb2")))
+            {
+                var file = Path.Combine(Path.GetDirectoryName(_file) ?? string.Empty, prefix + _file);
+                using var reader = new StreamReader(entry.Open());
+                var text = await reader.ReadToEndAsync();
+                if (File.Exists(file))
+                {
+                    File.SetAttributes(file, FileAttributes.Normal);
+                    File.Delete(file);
+                }
+                await File.WriteAllTextAsync(file, text);
+                files.Add(file);
+            }
+
+            return files;
+        }
+        public static void RestoreFile(string file)
+        {
+            var restoredFile = file.Substring(0, file.Length - 1); // removes _
+            if (File.Exists(restoredFile))
+            {
+                File.SetAttributes(restoredFile, FileAttributes.Normal);
+            }
+            File.Move(file, restoredFile, true);
+        }
+    }
+}
